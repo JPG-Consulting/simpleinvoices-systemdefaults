82ff81fab7d437db004a3b5ace0854fefa77611e
GarlicFramework <unaputacuenta@hotmail.com>
2015-10-20 20:06:10 +0200

    first commit

---
+++ Module.php
@@ -0,0 +1,26 @@
+<?php
+namespace SimpleInvoices\SystemDefaults;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+
+class Module implements AutoloaderProviderInterface
+{
+    /**
+     * Return an array for passing to Zend\Loader\AutoloaderFactory.
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/',
+                ),
+            ),
+        );
+    }
+}
