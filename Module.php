872dde135d295f0d0ee862236356b7aecb5c3237
GarlicFramework <unaputacuenta@hotmail.com>
2015-10-20 20:17:40 +0200

    fix

--- Module.php
+++ Module.php
@@ -13,9 +13,6 @@ class Module implements AutoloaderProviderInterface
     public function getAutoloaderConfig()
     {
         return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/',
